421a002dead47d37280240a9c6a114eb4d3e437d
Tianyu Liu <iboxty@gmail.com>
2018-04-11 18:18:46 +0800

    increase max size of decoding

--- SeqUnit.py
+++ SeqUnit.py
@@ -15,7 +15,7 @@ from OutputUnit import OutputUnit
 class SeqUnit(object):
     def __init__(self, batch_size, hidden_size, emb_size, field_size, pos_size, source_vocab, field_vocab,
                  position_vocab, target_vocab, field_concat, position_concat, fgate_enc, dual_att,
-                 encoder_add_pos, decoder_add_pos, learning_rate, scope_name, name, start_token=2, stop_token=2, max_length=64):
+                 encoder_add_pos, decoder_add_pos, learning_rate, scope_name, name, start_token=2, stop_token=2, max_length=150):
         '''
         batch_size, hidden_size, emb_size, field_size, pos_size: size of batch; hidden layer; word/field/position embedding
         source_vocab, target_vocab, field_vocab, position_vocab: vocabulary size of encoder words; decoder words; field types; position
